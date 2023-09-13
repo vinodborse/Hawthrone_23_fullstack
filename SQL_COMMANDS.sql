88f8ba0c436680de65831be2093758c64f80fda8
Vinodkumar Borse <borse002@gmail.com>
2023-09-13 09:41:33 +0530

    Add files via upload

--- SQL_COMMANDS.sql
+++ SQL_COMMANDS.sql
@@ -4,12 +4,39 @@ SHOW DATABASES;
 
 USE <database_name>;
 
+DROP DATABASE <database_name>;
+
+
+--------------------------
+
+
 CREATE TABLE <table_name>(<column_name> <data_type> );
 
 SELECT <columan_name> FROM <table_name>;
 
 INSERT INTO <table_name>(<columan_name>) VALUE (<columan_value>) 
- 
+
+DROP TABLE <table_name>;
+
+TRUNCATE TABLE <table_name>;
+
+	ALTER TABLE <table_name> ADD <new_column_name> <data_type>;
+	ALTER TABLE <table_name> DROP <column_name>;
+	ALTER TABLE <table_name> MODIFY <column_name> <new_data_type>;
+	ALTER TABLE <table_name> RENAME COLUMN <old_column_name> TO <new_column_name>;
+
+ -- CONSTRAINTS
+	UNIQUE
+	NOT NULL
+	PRIMARY KEY ---> UNIQUE + NOT NULL
+	FOREIGN
+	DEFAULT
+	AUTO_INCREMENT
+
+CREATE TABLE <table_name>(<column_name> <data_type> <constraint>);
+CREATE TABLE student(rollno int UNIQUE NOT NULL); 	
+
+FOREIGN KEY (<column_name>) REFERENCES <parent_table_name> (<parent_column_name>);
 
 -------------------------------
 CREATE DATABASE testingshastra;
