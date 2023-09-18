a71705fe0c4e532d0c6bb5309893fdecb75d317f
Vinodkumar Borse <borse002@gmail.com>
2023-09-18 11:18:08 +0530

    Add files via upload

--- SQL_COMMANDS.sql
+++ SQL_COMMANDS.sql
@@ -48,3 +48,66 @@ CREATE TABLE student(rollno int, name varchar(50), address varchar(50));
 SELECT rollno, name, address FROM student;
 
 INSERT INTO student(rollno, name, address) VALUE (1, 'Hukalala', 'Bavuma');
+
+
+
+
+-------------------------
+DELETE FROM <table_name> WHERE <condition>
+DELETE FROM <table_name> WHERE <column_name> = <column_value>;
+UPDATE <table_name> SET <column_name> = <new_value> WHERE <column_name> = <column_value>;
+
+IN
+OR
+AND
+
+LIMIT <count>
+ORDER BY <column_name> ASC/DESC;
+
+COUNT()
+MIN()
+MAX()
+AVG()
+
+<column_name> % 2 = 0 ----> This is for even numbers 
+<column_name> % 2 != 0 ----> This is for odd numbers
+
+
+
+DELETE FROM student WHERE name = 'Gaurav';
+DELETE FROM student WHERE rollno = 5;
+
+UPDATE FROM <table_name> WHERE <column_name> = 'Chameli'
+UPDATE student SET name = 'Sonchidi' WHERE name = 'Chameli';
+
+
+SELECT * FROM student;
+SELECT * FROM student WHERE rollno = 5;
+SELECT name FROM student WHERE rollno = 5;
+
+
+
+
+
+SELECT * FROM student ORDER BY rollno DESC LIMIT 3;
+
+
+SELECT * FROM student ORDER BY rollno DESC LIMIT 3 ; 
+
+
+rollno	name		marathi		gujarati
+ 8		Sonchidi	  67			45
+
+
+
+
+
+
+
+
+
+
+
+
+
+
